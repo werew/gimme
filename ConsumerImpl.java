1b41ae61d5520812e3f4010be46856ef5d852a14
CONIGLIO Luigi <coniglio@turing.ad.unistra.fr>
2017-04-06 18:40:46 +0200

    Consumer command line options

--- ConsumerImpl.java
+++ ConsumerImpl.java
@@ -10,8 +10,13 @@ import Gimme.Coordinator;
 import Gimme.CoordinatorHelper;
 import Gimme.ConsumerPOA;
 import Gimme.ConsumerHelper;
+import Gimme.GameInfos;
+import org.apache.commons.cli.*;
 
 public class ConsumerImpl extends ConsumerPOA {
+
+    boolean taketurns = false;
+    private boolean human = false;
     Consumer mycons;
     Coordinator coordinator;
     ThreadRun orbthread;
@@ -25,15 +30,59 @@ public class ConsumerImpl extends ConsumerPOA {
         return 0;
     }
     
+    public void setHuman(){
+        taketurns = true;
+        human = true;
+    }
+    
+    private static Options getOptions(){
+
+        Option taketurns = new Option("t","taketurns",false, 
+            "For games which are played in turns");
+        Option human = new Option("h","human",false, 
+            "Activate user interaction (for games played in turns)");
+
+        Options options = new Options();
+        options.addOption(taketurns); 
+        options.addOption(human); 
+
+        return options;
+    }
+
+    private static void printUsage(Options options, int exitcode){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java ConsumerImpl [OPTIONS] <Name Server> <Port>", options);
+        System.exit(exitcode);
+    }
 
     public static void main(String args[]){
 
-        ConsumerImpl consumer = null;
+        Options options = getOptions();
+        CommandLine cmd = null;
+        String[] argz = null;
 
-        if (args.length != 2){
-            System.out.println("Usage : java ConsumerImpl" + " <machineServeurDeNoms>" + " <No Port>") ;
-            return ;
-        } 
+        try {
+            /* Parse options */
+            CommandLineParser parser = new DefaultParser();
+            cmd = parser.parse(options, args);
+            
+            /* Check arguments */
+            argz = cmd.getArgs();
+            if (argz.length < 2) throw new ParseException("Argument missing");
+
+        } catch (ParseException e) {
+
+            System.out.println("\nERROR: "+e.getMessage()+"\n");
+            printUsage(options,1);
+        }
+
+
+        /* Create and init consumer */
+        ConsumerImpl consumer = new ConsumerImpl();
+
+        if (cmd.hasOption('t')) consumer.taketurns = true;
+        if (cmd.hasOption('h')) consumer.setHuman();
+
 
         try {
             CorbaManager cm = new CorbaManager(args[0],args[1]);

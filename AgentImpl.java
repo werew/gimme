fab83e58c1d168653a326f925bde7a8ce78015f9
CONIGLIO Luigi <coniglio@turing.ad.unistra.fr>
2017-04-08 13:18:39 +0200

    Adding empty AgentImpl class

---
+++ AgentImpl.java
@@ -0,0 +1,18 @@
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import java.util.*;
+import java.io.*;
+import java.io.IOException;
+import org.omg.CosNaming.*;
+import Gimme.Producer;
+import Gimme.Agent;
+import Gimme.Coordinator;
+import Gimme.CoordinatorHelper;
+import Gimme.AgentPOA;
+import Gimme.AgentHelper;
+import Gimme.GameInfos;
+import org.apache.commons.cli.*;
+
+public class AgentImpl extends AgentPOA {
+
+}

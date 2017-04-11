9a704adc7d82590f208f4b6d2de8e0c55445a1a4
CONIGLIO Luigi <coniglio@uds-501591.ad.unistra.fr>
2017-04-11 10:19:08 +0200

    Get resource type as parameter

--- ProducerImpl.java
+++ ProducerImpl.java
@@ -11,6 +11,7 @@ import Gimme.CoordinatorHelper;
 import Gimme.ProducerOperations;
 import Gimme.ProducerHelper;
 import Gimme.Resource;
+import org.apache.commons.cli.*;
 
 public class ProducerImpl extends AgentImpl
 implements ProducerOperations {
@@ -31,12 +32,58 @@ implements ProducerOperations {
 
     public Resource queryResource(){ return resource; }
 
+    /**
+     * @brief Generate cli options
+     * 
+     * Retrieve an object Options containing all the
+     * command line options used by the consumer
+     * executable and their description
+     *
+     * @return an object of type Options
+     */
+    private static Options getOptions(){
+
+        Option maxamount = new Option("m","max",false, 
+            "Max amount of the resource");
+
+        Options options = new Options();
+        options.addOption(maxamount); 
+
+        return options;
+    }
+
+
+    /**
+     * @brief Print usage and exit the program
+     * @param options options to use for the usage description 
+     * @param exitcode exit code of the program
+     */
+    private static void printUsage(Options options, int exitcode){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java ProducerImpl [OPTIONS] <Name Server> <Port> <Type Resource>", options);
+        System.exit(exitcode);
+    }
+
     public static void main(String args[]){
 
-        if (args.length != 2){
-            System.out.println("Usage : java ProducerImpl" + " <machineServeurDeNoms>" + " <No Port>") ;
-            return ;
-        } 
+        Options options = getOptions();
+        CommandLine cmd = null;
+        String[] argz = null;
+
+        try {
+            /* Parse options */
+            CommandLineParser parser = new DefaultParser();
+            cmd = parser.parse(options, args);
+            
+            /* Check arguments */
+            argz = cmd.getArgs();
+            if (argz.length < 3) throw new ParseException("Argument missing");
+
+        } catch (ParseException e) {
+
+            System.out.println("\nERROR: "+e.getMessage()+"\n");
+            printUsage(options,1);
+        }
 
         try {
             CorbaManager cm = new CorbaManager(args[0],args[1]);

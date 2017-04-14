8cd16267d3f195f821c09cc81b2816ca4c0b17ed
werew <luigi.coniglio@etu.unistra.fr>
2017-04-14 12:41:11 +0200

    Agents can store transactions

--- AgentImpl.java
+++ AgentImpl.java
@@ -8,6 +8,7 @@ import Gimme.Producer;
 import Gimme.Resource;
 import Gimme.Agent;
 import Gimme.Coordinator;
+import Gimme.Transaction;
 import Gimme.CoordinatorHelper;
 import Gimme.AgentPOA;
 import Gimme.AgentHelper;
@@ -17,6 +18,8 @@ import org.apache.commons.cli.*;
 public abstract class AgentImpl extends AgentPOA {
 
     protected String gameID;
+    protected HashMap<String,Transaction> transactions;
+    protected Date date;
 
     /* TODO use log class to implement msg types */
     public void logmsg(String msg, int type){
@@ -24,5 +27,22 @@ public abstract class AgentImpl extends AgentPOA {
     }
 
     public abstract Resource getResource(Resource request);
+
+    public AgentImpl(){
+        transactions = new HashMap<String,Transaction>();
+        date = new Date();
+    }
+
+    protected void addTransaction(String to, Resource r){
+        Transaction t = new Transaction();
+        t.timestamp = date.getTime();
+        t.id = gameID+"-"+transactions.size();
+        t.from = gameID;
+        t.to = to;
+        t.what = r;
+        transactions.put(t.id,t);
+    }
+
+
 
 }

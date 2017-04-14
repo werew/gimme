bad34d8f0408023f1b937c172e55f9ede9d8eb16
werew <luigi.coniglio@etu.unistra.fr>
2017-04-14 16:49:15 +0200

    Adding transaction types

--- Common.java
+++ Common.java
@@ -6,4 +6,10 @@ public class Common {
     public static final int FULL = 1;
     public static final int UNKNOWN_ERROR = 2;
 
+
+    /* Transaction types */
+    public static final int REQUEST;
+    public static final int QUERY;
+    public static final int PRODUCTION;
+
 }

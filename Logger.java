fdf6c472e5fb2c1bec028cd42c7649947f430ef3
werew <luigi.coniglio@etu.unistra.fr>
2017-04-29 23:36:25 +0200

    Adding class logger

---
+++ Logger.java
@@ -0,0 +1,79 @@
+import static java.lang.System.out;
+
+
+
+public class Logger {
+
+    // Commands
+    static final String CLEAR_SCREEN = "2J";
+    static final String CLEAR_LINE = "K";
+  
+    // Text attributes  
+    static final String OFF = "0";
+    static final String BOLD = "1";
+    static final String UNDERSCORE = "2";
+    static final String BLINK  = "3";
+     
+    // Foreground colors 
+    static final String FBLACK = "30";
+    static final String FRED = "31";
+    static final String FGREEN = "32";
+    static final String FYELLOW  = "33";
+    static final String FBLUE = "34";
+    static final String FMAGENTA = "35";
+    static final String FCYAN = "36";
+    static final String FWHITE = "37";
+
+    // Background colors 
+    static final String BBLACK = "40";
+    static final String BRED = "41";
+    static final String BGREEN = "42";
+    static final String BYELLOW  = "43";
+    static final String BBLUE = "44";
+    static final String BMAGENTA = "45";
+    static final String BCYAN = "46";
+    static final String BWHITE = "47";
+
+
+    static private String esc(String cmd){
+        return "\033["+cmd+"m";
+    }
+
+    static public void theme(String cmd){
+       out.println(esc(cmd)+esc(CLEAR_SCREEN));
+    }
+
+    static public void info(String msg){
+       out.println(esc(FCYAN)+msg+esc(OFF)); 
+    }
+
+    static public void warning(String msg){
+       out.println(esc(FYELLOW)+msg+esc(OFF)); 
+    }
+
+    static public void error(String msg){
+       out.println(esc(FRED)+msg+esc(OFF)); 
+    }
+
+    static public void success(String msg){
+       out.println(esc(FGREEN)+msg+esc(OFF)); 
+    }
+
+    static public String with(String msg, String cmd){
+       return "\033["+cmd+"m"+msg+"\033[0m";       
+    }
+
+    static public void reset(){
+       out.println("\033[0m");
+    }
+}
+    
+
+
+        
+
+    
+    
+    
+
+    
